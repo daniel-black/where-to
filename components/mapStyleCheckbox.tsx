e0bfcb328031b54203911f786cae05b3a5b24a0d
Dan <drblack651@gmail.com>
2022-07-23 00:27:06 -0500

    Fix enum type error to make TypeScript happy

--- components/mapStyleCheckbox.tsx
+++ components/mapStyleCheckbox.tsx
@@ -3,20 +3,19 @@ import { SelectMapStyleProps } from "./selectMapStyle";
 
 type MapStyleCheckboxProps = SelectMapStyleProps & { styleKey: MapStyleKey }
 
-const MapStyleCheckbox = (props: MapStyleCheckboxProps) => {
-  return (
-    <div>
-      <input 
-        type="radio" 
-        name='theme' 
-        value={MapStyle[props.styleKey]} 
-        id='Outdoors-theme' 
-        checked={props.mapStyle === MapStyle[props.styleKey]} 
-        onChange={(e) => props.handleChange(e.currentTarget.value)} 
-      />
-      <label htmlFor="outdoors-theme">{props.styleKey}</label>
-    </div>
-  )
-}
+const MapStyleCheckbox = (props: MapStyleCheckboxProps) => (
+  <div>
+    <input 
+      type="radio" 
+      name='theme' 
+      value={MapStyle[props.styleKey]} 
+      id='Outdoors-theme' 
+      checked={props.mapStyle === MapStyle[props.styleKey]} 
+      onChange={() => props.handleChange(MapStyle[props.styleKey])} 
+    />
+    <label htmlFor="outdoors-theme">{props.styleKey}</label>
+  </div>
+);
+
 
 export default MapStyleCheckbox;
