b08ca4efc28ef5418a40d7d20b2a7dfc91fd97c1
Dan <drblack651@gmail.com>
2022-07-23 00:32:12 -0500

    small refactoring of dumb components + updated README

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 - Define database schemas
 - Add basic routes for setting home location and desired move-to locations
 - Let users create notes for a place and be able to see them on the map at the set geography
+
+-----
+
+Current build of master branch hosted at: https://where-to-alpha.vercel.app/map
--- components/selectMapStyle.tsx
+++ components/selectMapStyle.tsx
@@ -6,22 +6,18 @@ export type SelectMapStyleProps = {
   handleChange: (style: MapStyle) => void
 }
 
-const SelectMapStyle = (props: SelectMapStyleProps) => {
-
-
-  return (
-    <details>
-      <summary>Map Theme</summary>
-      <MapStyleCheckbox {...props} styleKey='Dark' />
-      <MapStyleCheckbox {...props} styleKey='Light' />
-      <MapStyleCheckbox {...props} styleKey='Streets' />
-      <MapStyleCheckbox {...props} styleKey='Outdoors' />
-      <MapStyleCheckbox {...props} styleKey='Satellite' />
-      <MapStyleCheckbox {...props} styleKey='Navigation Day' />
-      <MapStyleCheckbox {...props} styleKey='Navigation Night' />
-      <MapStyleCheckbox {...props} styleKey='Satellite Streets' />
-    </details>
-  )
-}
+const SelectMapStyle = (props: SelectMapStyleProps) => (
+  <details>
+    <summary>Map Theme</summary>
+    <MapStyleCheckbox {...props} styleKey='Dark' />
+    <MapStyleCheckbox {...props} styleKey='Light' />
+    <MapStyleCheckbox {...props} styleKey='Streets' />
+    <MapStyleCheckbox {...props} styleKey='Outdoors' />
+    <MapStyleCheckbox {...props} styleKey='Satellite' />
+    <MapStyleCheckbox {...props} styleKey='Navigation Day' />
+    <MapStyleCheckbox {...props} styleKey='Navigation Night' />
+    <MapStyleCheckbox {...props} styleKey='Satellite Streets' />
+  </details>
+);
 
 export default SelectMapStyle;
