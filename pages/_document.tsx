27c81cec1600764a31a038aa67e1960a240e9968
Dan <drblack651@gmail.com>
2022-07-23 10:28:13 -0500

    Add _document page with base styles

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+type Props = {}
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className='bg-slate-900 min-h-full min-w-full'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
